1ed73337bac9a632295f359426b0f6b4b30fdfe3
北西 <northwest.output.official@gmail.com>
2023-01-13 16:51:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ fp.close()
 def testFunc():
   print('test done')
 
-def getValue(key):
-  return = eval_js('getValue({})'.format(key))
-
 # 出力場所作成 + 必要情報の入力
 display(HTML("<div id='colab_gui_main'></div>"))
 display(HTML('<style>' + (default_css) + '</style>'))
@@ -33,20 +30,25 @@ display(HTML('<script>' + (colab_gui_input_js) + '</script>'))
 class ColabGUIButton:
   def __init__(self, key):
     self.key = key
-    display(HTML("<script>const colabguibtn_" + self.key + " = new ColabGUIButton(" + self.key + ");</script>"))
+    display(HTML("<script>const colabguibtn_{} = new ColabGUIButton('{}');</script>".format(self.key, self.key)))
   def attachEvent(self, event):
     f_name = event.__name__
-    display(HTML("<script>colabguibtn_{}.setClickEvent({});</script>").format(self.key, 'n.' + f_name))
+    display(HTML("<script>colabguibtn_{}.setClickEvent('{}');</script>").format(self.key, 'n.' + f_name))
     output.register_callback('n.' + f_name, event)
   def removeEvent(self, event):
     f_name = event.__name__
-    display(HTML("<script>colabguibtn_{}.removeClickEvent({});</script>").format(self.key, 'n.' + f_name))
+    display(HTML("<script>colabguibtn_{}.removeClickEvent('{}');</script>").format(self.key, 'n.' + f_name))
 
 class ColabGUIInput:
   def __init__(self, key):
-    
+    self.key = key
+    display(HTML("<script>const colabguiinput_{} = new ColabGUIInput('{}');</script>".format(self.key, self.key)))    
+  def getValue(self):
+    js = "colabguiinput_{}.getValue();".format(self.key)
+    value = eval_js(js)
+    return value
 
-display(HTML("<script>const c_input = new ColabGUIInput('test_input');</script>"))
+cb = ColabGUIButton('test_btn')
+ci = ColabGUIInput('test_input')
 
-time.sleep(10)
-print(getValue('test_input'))
+print(ci.getValue())
